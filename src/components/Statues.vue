ad2d9ae7f81f5bf741d2f80b62169c539992f97e
Kkrisztian08 <k.krisztian0830@gmail.com>
2022-02-08 16:37:44 +0100

    szerkesztés kész

--- src/components/Statues.vue
+++ src/components/Statues.vue
@@ -1,43 +1,44 @@
 <template>
-  <div>
-      <h1>Statues</h1>
-      <table>
-        <thead>
-            <tr>
-                <th>Személy</th>
-                <th>Magasság</th>
-                <th>Ár</th>
-                <th>Műveletek</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="statue in statues" v-bind:key="statue.id">
-                <td>{{ statue.person }}</td>
-                <td>{{ statue.height }}</td>
-                <td>{{ statue.price }}</td>
-                <td>
-                    <button>Szerkesztés</button>
-                    <button @click="deleteStatue(statue.id)">Törlés</button>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <input type="text" v-model="statue.person">
-                </td>
-                <td>
-                    <input type="number" v-model="statue.height">
-                </td>
-                <td>
-                    <input type="number" v-model="statue.price">
-                </td>
-                <td>
-                    <button @click="newStatue">Mentés</button>
-                    <button>Mégse</button>
-                </td>
-            </tr>
-        </tbody>
-      </table>
-  </div>
+<div>
+        <h1>Statues</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Személy</th>
+                    <th>Magasság</th>
+                    <th>Ár</th>
+                    <th>Műveletek</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="statue in statues" v-bind:key="statue.id">
+                    <td>{{ statue.person }}</td>
+                    <td>{{ statue.height }}</td>
+                    <td>{{ statue.price }}</td>
+                    <td>
+                        <button @click="loadEditData(statue.id)">Szerkesztés</button>
+                        <button @click="deleteStatue(statue.id)">Törlés</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="text" v-model="statue.person">
+                    </td>
+                    <td>
+                        <input type="number" v-model="statue.height">
+                    </td>
+                    <td>
+                        <input type="number" v-model="statue.price">
+                    </td>
+                    <td>
+                        <button v-if="newData" @click="newStatue">Mentés</button>
+                        <button v-if="!newData" @click="saveEditedData">Mentés</button>
+                        <button @click="clearForm">Mégse</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
@@ -45,6 +46,7 @@ export default {
    name: 'Statues',
     data()  {
         return {
+            newData: true,
             statue: {
                 id: null,
                 person: '',
@@ -78,6 +80,25 @@ export default {
             });
             console.log(Response);
             await this.loadData();
+        },
+        async loadEditData(id) {
+            let Response = await fetch(`http://127.0.0.1:8000/api/statues/${id}`);
+            let data = await Response.json();
+            this.statue = {...data};
+            this.newData = false;
+        },
+        async saveEditedData() {
+            await fetch(`http://127.0.0.1:8000/api/statues/${this.statue.id}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify(this.statue) 
+            });
+            this.loadData();
+            this.clearForm();
+            this.newData = true;
         },
         clearForm() {
             this.statue = {
