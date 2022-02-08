cfe94e39eba9876cf90f78493d1da80a48685451
Kkrisztian08 <k.krisztian0830@gmail.com>
2022-02-08 15:25:31 +0100

    Szobrok klistázása (+ töröl és modosit gomb)

---
+++ src/components/Statues.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+      <h1>Statues</h1>
+      <table>
+        <thead>
+            <tr>
+                <th>Személy</th>
+                <th>Magasság</th>
+                <th>Ár</th>
+                <th>Műveletek</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="statue in statues" v-bind:key="statue.id">
+                <td>{{ statue.person }}</td>
+                <td>{{ statue.height }}</td>
+                <td>{{ statue.price }}</td>
+                <td>
+                    <button>Módosítás</button>
+                    <button>Törlés</button>
+                </td>
+            </tr>
+        </tbody>
+      </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Statues',
+  data()  {
+      return {
+        statues: []
+      }
+  },
+  methods: {
+      async loadData() {
+          let Response = await fetch('http://127.0.0.1:8000/api/statues');
+          let data = await Response.json();
+          this.statues = data;
+      }
+  },
+  mounted() {
+    this.loadData();
+  }
+}
+</script> 
